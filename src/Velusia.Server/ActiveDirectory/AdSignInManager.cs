6ce27521166aed9f232d687bb66f25c7f3f442e7
Lyubomir Lyubenov <lyubomir.lyubenov@procreditbank.bg>
2022-04-09 20:27:40 +0300

    format code

--- src/Velusia.Server/ActiveDirectory/AdSignInManager.cs
+++ src/Velusia.Server/ActiveDirectory/AdSignInManager.cs
@@ -13,125 +13,125 @@ using static OpenIddict.Abstractions.OpenIddictConstants;
 namespace Velusia.Server.ActiveDirectory
 {
     public class AdSignInManager : ISignInManager
-	{
-		private readonly IOptionsMonitor<AdSettings> _settings;
+    {
+        private readonly IOptionsMonitor<AdSettings> _settings;
         private readonly IOptionsMonitor<AuthenticationSettings> _authenticationSettings;
         private readonly IHttpContextAccessor _contextAccessor;
-		private readonly IAuthenticationService _authenticationService;
-		private readonly IUserManager _userManager;
-		private readonly ILogger _logger;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IUserManager _userManager;
+        private readonly ILogger _logger;
 
-		public AdSignInManager(
-			IOptionsMonitor<AdSettings> settings,
-			IOptionsMonitor<AuthenticationSettings> authenticationSettings,
-			IHttpContextAccessor contextAccessor,
-			IAuthenticationService authenticationService,
-			IUserManager userManager,
-			ILogger<AdSignInManager> logger)
-		{
-			_settings = settings;
+        public AdSignInManager(
+            IOptionsMonitor<AdSettings> settings,
+            IOptionsMonitor<AuthenticationSettings> authenticationSettings,
+            IHttpContextAccessor contextAccessor,
+            IAuthenticationService authenticationService,
+            IUserManager userManager,
+            ILogger<AdSignInManager> logger)
+        {
+            _settings = settings;
             _authenticationSettings = authenticationSettings;
             _contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
-			_authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
-			_userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
-			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-		}
-
-		public string AuthenticationScheme
-		{
-			get
-            {
-				return _authenticationSettings.CurrentValue.Scheme;
-			}
-		}
-
-		public async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent)
-		{
-			try
-			{
-				var settings = _settings.CurrentValue;
-				var user = await _userManager.GetUserAsync(userName, password);
-				if (user == null)
-				{
-					return new SignInResult("Invalid user name or password.");
-				}
-
-				if (settings.AllowGroups?.Length > 0 && !settings.AllowGroups[0].Equals("*", StringComparison.OrdinalIgnoreCase) && !user.Groups.Any(groupName => settings.AllowGroups.Contains(groupName, StringComparer.InvariantCultureIgnoreCase)))
-				{
-					return new SignInResult("User dosen't have permisions to login.");
-				}
-
-				if (!user.Enabled)
-				{
-					return new SignInResult("User is disabled.");
-				}
-
-				if (user.LockedOut)
-				{
-					return SignInResult.LockedOut;
-				}
-
-				var principal = await CreateUserPrincipalAsync(user);
-
-				var properties = new AuthenticationProperties
-				{
-					IsPersistent = isPersistent
-				};
-
-				await _authenticationService.SignInAsync(_contextAccessor.HttpContext, AuthenticationScheme, principal, properties);
-
-				return SignInResult.Success;
-			}
-			catch (Exception ex)
-            {
-				_logger.LogError(ex, ex.Message);
-				return SignInResult.Failed;
-			}
-		}
-
-		public Task<ClaimsPrincipal> CreateUserPrincipalAsync(User user)
-        {
-			var identity = new ClaimsIdentity(AuthenticationScheme, Claims.Name, Claims.Role);
-			identity.AddClaim(new Claim(Claims.AuthenticationMethodReference, AuthenticationScheme));
-			identity.AddClaim(new Claim(Claims.Subject, user.Id));
-			identity.AddClaim(new Claim(Claims.Name, user.DisplayName));
-			identity.AddClaim(new Claim(Claims.Nickname, user.Name));
-
-			// if (UserManager.SupportsUserSecurityStamp) Claims.UpdatedAt
-			// {
-			// 	ClaimsIdentity claimsIdentity = id;
-			// 	string securityStampClaimType = Options.ClaimsIdentity.SecurityStampClaimType;
-			// 	claimsIdentity.AddClaim(new Claim(securityStampClaimType, await UserManager.GetSecurityStampAsync(user)));
-			// }
-
-			return Task.FromResult(new ClaimsPrincipal(identity));
+            _authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-		public Task<bool> CanSignInAsync(User user)
-		{
-			if (!user.Enabled || user.LockedOut)
-			{
-				_logger.LogWarning("User {userId} cannot sign, it is disabled.", user.Name);
-				return Task.FromResult(false);
-			}
+        public string AuthenticationScheme
+        {
+            get
+            {
+                return _authenticationSettings.CurrentValue.Scheme;
+            }
+        }
 
-			return Task.FromResult(true);
-		}
+        public async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent)
+        {
+            try
+            {
+                var settings = _settings.CurrentValue;
+                var user = await _userManager.GetUserAsync(userName, password);
+                if (user == null)
+                {
+                    return new SignInResult("Invalid user name or password.");
+                }
 
-		public Task SignOutAsync()
-		{
-			return _authenticationService.SignOutAsync(
-				_contextAccessor.HttpContext,
-				AuthenticationScheme,
-				null);
-		}
+                if (settings.AllowGroups?.Length > 0 && !settings.AllowGroups[0].Equals("*", StringComparison.OrdinalIgnoreCase) && !user.Groups.Any(groupName => settings.AllowGroups.Contains(groupName, StringComparer.InvariantCultureIgnoreCase)))
+                {
+                    return new SignInResult("User dosen't have permisions to login.");
+                }
 
-		public async Task<Claim[]> GetRolesAsync(User user)
-		{
-			return (await _userManager.GetRolesAsync(user))
-				.Select(role => new Claim(Claims.Role, role))
-				.ToArray();
-		}
-	}
+                if (!user.Enabled)
+                {
+                    return new SignInResult("User is disabled.");
+                }
+
+                if (user.LockedOut)
+                {
+                    return SignInResult.LockedOut;
+                }
+
+                var principal = await CreateUserPrincipalAsync(user);
+
+                var properties = new AuthenticationProperties
+                {
+                    IsPersistent = isPersistent
+                };
+
+                await _authenticationService.SignInAsync(_contextAccessor.HttpContext, AuthenticationScheme, principal, properties);
+
+                return SignInResult.Success;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return SignInResult.Failed;
+            }
+        }
+
+        public Task<ClaimsPrincipal> CreateUserPrincipalAsync(User user)
+        {
+            var identity = new ClaimsIdentity(AuthenticationScheme, Claims.Name, Claims.Role);
+            identity.AddClaim(new Claim(Claims.AuthenticationMethodReference, AuthenticationScheme));
+            identity.AddClaim(new Claim(Claims.Subject, user.Id));
+            identity.AddClaim(new Claim(Claims.Name, user.DisplayName));
+            identity.AddClaim(new Claim(Claims.Nickname, user.Name));
+
+            // if (UserManager.SupportsUserSecurityStamp) Claims.UpdatedAt
+            // {
+            // 	ClaimsIdentity claimsIdentity = id;
+            // 	string securityStampClaimType = Options.ClaimsIdentity.SecurityStampClaimType;
+            // 	claimsIdentity.AddClaim(new Claim(securityStampClaimType, await UserManager.GetSecurityStampAsync(user)));
+            // }
+
+            return Task.FromResult(new ClaimsPrincipal(identity));
+        }
+
+        public Task<bool> CanSignInAsync(User user)
+        {
+            if (!user.Enabled || user.LockedOut)
+            {
+                _logger.LogWarning("User {userId} cannot sign, it is disabled.", user.Name);
+                return Task.FromResult(false);
+            }
+
+            return Task.FromResult(true);
+        }
+
+        public Task SignOutAsync()
+        {
+            return _authenticationService.SignOutAsync(
+                _contextAccessor.HttpContext,
+                AuthenticationScheme,
+                null);
+        }
+
+        public async Task<Claim[]> GetRolesAsync(User user)
+        {
+            return (await _userManager.GetRolesAsync(user))
+                .Select(role => new Claim(Claims.Role, role))
+                .ToArray();
+        }
+    }
 }
 #pragma warning restore CA1416 // Validate platform compatibility
--- src/Velusia.Server/ActiveDirectory/AdUserManager.cs
+++ src/Velusia.Server/ActiveDirectory/AdUserManager.cs
@@ -12,104 +12,104 @@ using static OpenIddict.Abstractions.OpenIddictConstants;
 #pragma warning disable CA1416 // Validate platform compatibility
 namespace Velusia.Server.ActiveDirectory
 {
-	// using (var p = Principal.FindByIdentity(c, IdentityType.SamAccountName, user))
-	// using (var p = Principal.FindByIdentity(c, IdentityType.SamAccountName, "DOMAIN\\" + user))
-	// using (var p = Principal.FindByIdentity(c, IdentityType.UserPrincipalName, user + "@domain.com"))
-	public class AdUserManager : IUserManager
-	{
-		private readonly IOptionsMonitor<AdSettings> _settings;
-		private readonly ILogger _logger;
+    // using (var p = Principal.FindByIdentity(c, IdentityType.SamAccountName, user))
+    // using (var p = Principal.FindByIdentity(c, IdentityType.SamAccountName, "DOMAIN\\" + user))
+    // using (var p = Principal.FindByIdentity(c, IdentityType.UserPrincipalName, user + "@domain.com"))
+    public class AdUserManager : IUserManager
+    {
+        private readonly IOptionsMonitor<AdSettings> _settings;
+        private readonly ILogger _logger;
 
-		public AdUserManager(IOptionsMonitor<AdSettings> settings, ILogger<AdUserManager> logger)
-		{
-			_settings = settings;
-			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-		}
+        public AdUserManager(IOptionsMonitor<AdSettings> settings, ILogger<AdUserManager> logger)
+        {
+            _settings = settings;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-		public Task<User> GetUserAsync(string userName, string password)
-	{
-			User adUser = null;
-			if (!(string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password)))
-			{
-				try
-				{
+        public Task<User> GetUserAsync(string userName, string password)
+        {
+            User adUser = null;
+            if (!(string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password)))
+            {
+                try
+                {
                     using var c = new PrincipalContext(_settings.CurrentValue.ContextType, _settings.CurrentValue.Server, null, ContextOptions.Negotiate, userName, password);
                     using var user = Principal.FindByIdentity(c, IdentityType.SamAccountName, userName) as UserPrincipal;
                     adUser = CreateUserFromPrincipal(user);
                 }
-				catch (DirectoryServicesCOMException ex) when (ex.ExtendedError == -2146893044 || ex.ErrorCode == -2147023570)
-				{
-					// The user name or password is incorrect.
-					_logger.LogInformation(ex, ex.Message);
-				}
-				catch (Exception ex)
-				{
-					// PrincipalServerDownException
-					_logger.LogError(ex, ex.Message);
-				}
-			}
+                catch (DirectoryServicesCOMException ex) when (ex.ExtendedError == -2146893044 || ex.ErrorCode == -2147023570)
+                {
+                    // The user name or password is incorrect.
+                    _logger.LogInformation(ex, ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    // PrincipalServerDownException
+                    _logger.LogError(ex, ex.Message);
+                }
+            }
 
-			return Task.FromResult(adUser);
-		}
+            return Task.FromResult(adUser);
+        }
 
-		public Task<User> GetUserAsync(ClaimsPrincipal principal)
+        public Task<User> GetUserAsync(ClaimsPrincipal principal)
         {
-			User adUser = null;
-			if (principal?.Identity?.IsAuthenticated == true)
+            User adUser = null;
+            if (principal?.Identity?.IsAuthenticated == true)
             {
-				try
-				{
+                try
+                {
                     using var context = new PrincipalContext(_settings.CurrentValue.ContextType, _settings.CurrentValue.Server, null, ContextOptions.Negotiate);
                     using var user = Principal.FindByIdentity(context, IdentityType.UserPrincipalName, principal.FindFirstValue(Claims.Nickname)) as UserPrincipal;
                     adUser = CreateUserFromPrincipal(user);
                 }
-				catch (Exception ex)
-				{
-					_logger.LogError(ex, ex.Message);
-				}
-			}
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                }
+            }
 
-			return Task.FromResult(adUser);
-		}
+            return Task.FromResult(adUser);
+        }
 
-		private static User CreateUserFromPrincipal(UserPrincipal principal)
-		{
-			if (principal == null)
-			{
-				return null;
-			}
+        private static User CreateUserFromPrincipal(UserPrincipal principal)
+        {
+            if (principal == null)
+            {
+                return null;
+            }
 
-			var entry = principal.GetUnderlyingObject() as DirectoryEntry;
+            var entry = principal.GetUnderlyingObject() as DirectoryEntry;
 
-			return new User
-			{
-				Id = principal.Sid.ToString(),
-				Name = principal.UserPrincipalName,
-				DisplayName = principal.DisplayName,
-				Enabled = principal.Enabled == true,
-				LockedOut = principal.IsAccountLockedOut(),
-				EmailAddress = principal.EmailAddress,
-				PhoneNumber = principal.VoiceTelephoneNumber,
-				Changed = Convert.ToDateTime(entry.Properties["whenChanged"].Value),
-				PasswordChanged = principal.LastPasswordSet,
-				Groups = principal.GetGroupList(),
-			};
-		}
+            return new User
+            {
+                Id = principal.Sid.ToString(),
+                Name = principal.UserPrincipalName,
+                DisplayName = principal.DisplayName,
+                Enabled = principal.Enabled == true,
+                LockedOut = principal.IsAccountLockedOut(),
+                EmailAddress = principal.EmailAddress,
+                PhoneNumber = principal.VoiceTelephoneNumber,
+                Changed = Convert.ToDateTime(entry.Properties["whenChanged"].Value),
+                PasswordChanged = principal.LastPasswordSet,
+                Groups = principal.GetGroupList(),
+            };
+        }
 
-		public Task<string[]> GetRolesAsync(User user)
-		{
-			string[] roles = null;
-			if (user.Groups?.Length > 0 && _settings.CurrentValue.Roles?.Length > 0)
-			{
-				roles = _settings.CurrentValue
-					.Roles
-					.Where(r => user.Groups.Contains(r.GroupName, StringComparer.InvariantCultureIgnoreCase))
-					.Select(r => r.Name)
-					.ToArray();
-			}
+        public Task<string[]> GetRolesAsync(User user)
+        {
+            string[] roles = null;
+            if (user.Groups?.Length > 0 && _settings.CurrentValue.Roles?.Length > 0)
+            {
+                roles = _settings.CurrentValue
+                    .Roles
+                    .Where(r => user.Groups.Contains(r.GroupName, StringComparer.InvariantCultureIgnoreCase))
+                    .Select(r => r.Name)
+                    .ToArray();
+            }
 
-			return Task.FromResult(roles ?? Array.Empty<string>());
-		}
-	}
+            return Task.FromResult(roles ?? Array.Empty<string>());
+        }
+    }
 }
 #pragma warning restore CA1416 // Validate platform compatibility
